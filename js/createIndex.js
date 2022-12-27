343faf2a0fb835006efc98a33a79ed0ff710c761
Mitsworth SCZD <42417471+SCZD@users.noreply.github.com>
2022-12-27 23:26:49 +0100

    First Commit

---
+++ js/createIndex.js
@@ -0,0 +1,62 @@
+
+function addLink(...args){
+    var nodeLink = document.createElement('a');
+    nodeLink.href = '#' + args[1].trim();
+    nodeLink.textContent = args[1].trim();
+    args[0].appendChild(nodeLink)
+    return args[0];
+}
+
+function createSection(){
+    /* Se podria usar el querySelector que te devuelve el primero ya que por sentido solo DEBERIA haber UNO */
+    var contentTitle = arguments[0].getElementsByClassName('content-item-title')[0].textContent;
+    var nodeContentTitle = document.createElement('li');
+    nodeContentTitle = addLink(nodeContentTitle, contentTitle);
+    var nodesContentSubTitle = arguments[0].getElementsByClassName('content-item-subtitle');
+    var nodeOrderedList = document.createElement('ol');
+    for (const iterator of nodesContentSubTitle) {
+        var listItem = document.createElement('li');
+        listItem = addLink(listItem, iterator.textContent);
+        nodeOrderedList.appendChild(listItem);
+    }
+    return [nodeContentTitle, nodeOrderedList];
+}
+
+function createIndex(contentItem, index){
+    var nodeIndex = document.createElement('ol');
+    for (const item of contentItem) {
+        createSection(item).forEach( bundle => {
+            nodeIndex.appendChild(bundle);
+        });
+    }
+    index.appendChild(nodeIndex);
+}
+
+function addID(nodesContentItem){
+    for (const item of nodesContentItem) {
+        var nodesContentTitle = item.getElementsByClassName('content-item-title');
+        Array.from(nodesContentTitle).forEach(title =>{
+            title.id = title.textContent.trim();
+        });
+
+        var nodesContentSubTitle = item.getElementsByClassName('content-item-subtitle');
+        Array.from(nodesContentSubTitle).forEach(subTitle =>{
+            subTitle.id = subTitle.textContent.trim();
+        });
+    }
+    /* No se puede getElements de una HTMLCollection tiene que ser de un solo elemento*/;
+    /* ILEGAL => var nodeSubTitle = nodesContentItem.getElementsByClassName('content-subtitle');*/
+}
+
+function defer(){
+    if(document.readyState == 'complete'){
+        /* Los trigers tienen que estar cargados */
+        var nodesContentItem = document.getElementsByClassName('content-item');
+        var index = document.getElementById('index');
+        createIndex(nodesContentItem, index);
+
+        addID(nodesContentItem);
+    }
+}
+
+document.addEventListener('readystatechange',defer);
