afcfea340e5086602a01307083dca64a255dba8a
Mitsworth SCZD <42417471+SCZD@users.noreply.github.com>
2022-12-31 00:55:28 +0100

    Lateral Menu
    Beta lateral menu fix [scroll, reverse function]

--- js/createIndex.js
+++ js/createIndex.js
@@ -12,8 +12,10 @@ function createSection(){
     var contentTitle = arguments[0].getElementsByClassName('content-item-title')[0].textContent;
     var nodeContentTitle = document.createElement('li');
     nodeContentTitle = addLink(nodeContentTitle, contentTitle);
+
     var nodesContentSubTitle = arguments[0].getElementsByClassName('content-item-subtitle');
     var nodeOrderedList = document.createElement('ol');
+
     for (const iterator of nodesContentSubTitle) {
         var listItem = document.createElement('li');
         listItem = addLink(listItem, iterator.textContent);
@@ -48,14 +50,48 @@ function addID(nodesContentItem){
     /* ILEGAL => var nodeSubTitle = nodesContentItem.getElementsByClassName('content-subtitle');*/
 }
 
+
+
 function defer(){
     if(document.readyState == 'complete'){
         /* Los trigers tienen que estar cargados */
         var nodesContentItem = document.getElementsByClassName('content-item');
-        var index = document.getElementById('index');
-        createIndex(nodesContentItem, index);
-
+        var indexContent = document.getElementById('index-content');
+        createIndex(nodesContentItem, indexContent);
         addID(nodesContentItem);
+
+        function change(){
+            index = document.getElementById('index');
+            indexTrigger = document.getElementById('index-trigger');
+            title = document.getElementById('title');
+
+            indexContent.style.padding = '5px 30px 5px 5px';
+            indexTrigger.style.width = 30 + 'px';
+            indexTrigger.style.backgroundColor = '#66512a';
+            var regresoAlFuturo = indexContent.offsetHeight;
+
+            index.addEventListener('mouseover', ()=>{index.style.left = 0;});
+            index.addEventListener('mouseout', ()=>{index.style.left = (-index.offsetWidth + indexTrigger.offsetWidth) + 'px';});
+            
+            index.style.backgroundColor = '#1a1a1a';
+            index.style.position = 'fixed';
+            index.style.top = title.offsetHeight + 'px';
+            index.style.left = (-index.offsetWidth) + 'px';
+            index.style.height = '80vh';
+            index.style.display = 'flex';
+            index.style.overflow = 'auto';
+            indexTrigger.style.height = regresoAlFuturo + 'px';
+
+            index.style.borderTopRightRadius = '10px';
+            index.style.borderBottomRightRadius = '10px';
+        }
+        
+        document.addEventListener('scroll', function hola(e) {
+            if(window.scrollY > 600){
+                change();
+                e.target.removeEventListener('scroll',hola);
+            }
+        });
     }
 }
 
